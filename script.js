a2f3696bd5090a26c51dc8f63fe89afc0160bc44
Tsaone <tsaoneb3@gmail.com>
2023-08-30 10:59:53 +0200

    completed the JS for the convesions

--- script.js
+++ script.js
@@ -8,5 +8,20 @@ let convertFromKg = () => {
     ozRef.value = (kg * 35.274).toFixed(2);
 }
 
+let convertFromOz = () => {
+    let oz = ozRef.value;
+    lbRef.value = (oz / 16).toFixed(2);
+    kgRef.value = (oz / 35.274).toFixed(2);
+}
+
+let convertFromLb = () => {
+    let lb = lbRef.value;
+    kgRef.value = (lb / 2.205).toFixed(2);
+    ozRef.value = (lb * 16).toFixed(2);
+}
+
+
 kgRef.addEventListener("input", convertFromKg);
+lbRef.addEventListener("input", convertFromLb);
+ozRef.addEventListener("input", convertFromOz);
 window.addEventListener("load", convertFromKg);
